c3d357e51169e946fb975c4050d79c5fe418fd90
HarshitsSri <srivastavh757@gmail.com>
2024-10-18 13:08:30 +0530

    js work

---
+++ jsWORK/index.js
@@ -0,0 +1,13 @@
+let a=12;
+console.log("hello");
+if(a>10){
+    var b=20;
+    console.log("a inside:"+a);
+
+}
+console.log("a outside:"+a);
+function fun(a,b){
+    console.log("hiii");
+
+}
+fun();//dynamic language as it also calls funtions with arguments when no arguments is called
