093db6cdf4ba83d43e25a6175ca9bda3630c5f32
HarshitsSri <srivastavh757@gmail.com>
2024-12-04 13:09:48 +0530

    react work

---
+++ REACTWORK/reactviteapp/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Student from './Student'
+
+
+function App() {
+  return (
+    <div>
+    <div>hello</div>
+    <div>
+      <Student name ="harshit srivastava" roll="22222" college="abes"/>
+    </div>
+  </div>
+
+  )
+}
+export default App
