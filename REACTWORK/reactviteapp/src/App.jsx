09fd80b8eea703c9b3fc8792e9c4d541aa31e188
HarshitsSri <srivastavh757@gmail.com>
2024-12-06 13:04:04 +0530

    use state

--- REACTWORK/reactviteapp/src/App.jsx
+++ REACTWORK/reactviteapp/src/App.jsx
@@ -1,16 +1,54 @@
 import React from 'react'
 import Student from './Student'
-
+import StateHandling from './StateHandling'
 
 function App() {
+  // const studentdata= [{
+  //   image:"https://missionfinancialservices.net/wp-content/uploads/2019/04/aprilblog2.jpg",
+  //   name:"Rahul Kumar",
+  //   roll:"23456",
+  //   college:"Abes Engineering College"
+  // },
+  // {
+  //   image:"https://tse2.mm.bing.net/th?id=OIP.5F3SyVGz57QTefASUtBtpgHaHa&pid=Api&P=0&h=180",
+  //   name:"Arjun Kumar",
+  //   roll:"123456",
+  //   college:"Abes Engineering College"
+
+  // },
+  // {
+  //   image:"https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png",
+  //   name:"Harshit Kumar",
+  //   roll:"23456",
+  //   college:"Abes Engineering College"
+  // }
+  // ]
   return (
     <div>
-    <div>hello</div>
-    <div>
-      <Student name ="harshit srivastava" roll="22222" college="abes"/>
-    </div>
-  </div>
+      <div>
+        <h2 style={{color:'red'}}>hello react js</h2>
+        <div>
+          <StateHandling />
+          
+          {/* <Student data={studentdata} /> */}
+          {/* {
+            studentdata.map((ele)=>{
 
+              return (
+                <Student data={ele} />
+
+              )
+            })
+          } */}
+        </div>
+        <div>
+          {/* <Student image={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100} />} name="Rahul Kumar" roll="2346" college="ABES Engineering college" /> */}
+        </div>
+
+      </div>
+      
+    </div>
   )
 }
+
 export default App
