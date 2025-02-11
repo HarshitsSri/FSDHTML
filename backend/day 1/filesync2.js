7c2768e6f88fd7e28d69dea3b56cd5805c703270
HarshitsSri <srivastavh757@gmail.com>
2025-02-11 15:27:46 +0530

    backend first day of doing file operation

---
+++ backend/day 1/filesync2.js
@@ -0,0 +1,11 @@
+const{ myReadFile,myWriteFile,myAppendFile,myDeleteFile,username}=require("./filesync1");
+myReadFile();
+const data ="my fsd class";
+myWriteFile(data);
+myReadFile();
+myAppendFile(data);
+myReadFile();
+myDeleteFile("dummy.txt");
+console.log("username=",username);
+
+
